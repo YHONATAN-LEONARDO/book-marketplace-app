547bd10f7f63a90d311cc66ee61c6645dc7362f9
Yhonatan Leonardo Mamani Torrez <yonconortorez@gmail.com>
2025-05-02 14:38:02 -0400

    terminado

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import Navegador from "@/components/molecules/Navegador";
-import Image from "next/image";
 
 export default function Home() {
   return (
--- components/molecules/Navegador.tsx
+++ components/molecules/Navegador.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import { Roboto } from "next/font/google";  
+
+
+const roboto = Roboto({
+  variable: "--font-roboto",
+  subsets: ["latin"],
+});
 
 const Navegador = () => {
   return (
-    <div>hola bebe</div>
-  )
-}
+    <div className={`navegador ${roboto.variable}`}>
+      <h2>Explore Market</h2>
+      <div className="navegador__icons">
+        <Image className="im" src="/campana.png" alt="campana" width={50} height={50} />
+        <Image className="im" src="/lupa.png" alt="lupa" width={50} height={50} />
+      </div>
+    </div>
+  );
+};
 
-export default Navegador
+export default Navegador;
